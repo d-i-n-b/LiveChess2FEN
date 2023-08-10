c1572103440fd838f3ea1ec0782b5afe96862a2b
DIEGO NAVARRO <dgnavarro@uc.cl>
2023-08-10 04:16:47 -0400

    .

--- lc2fen/predict_board.py
+++ lc2fen/predict_board.py
@@ -13,7 +13,7 @@ import onnxruntime
 from keras.models import load_model
 from keras.utils.image_utils import load_img, img_to_array
 import chess
-from PIL import Image, ImageOps
+from PIL import Image
 
 try:
     import pycuda.driver as cuda
@@ -67,7 +67,7 @@ def save_prediction_image(fen, filepath):
     for square, piece in board.piece_map().items():
         x = (square % 8) * square_size
         y = (7 - square // 8) * square_size  # 7 - because we're starting from the top
-        mask = ImageOps.invert(piece_images[piece.symbol()].convert("L"))
+        mask = piece_images[piece.symbol()].split()[3]  # Get the alpha channel directly
         board_img.paste(piece_images[piece.symbol()], (x, y), mask)
 
     board_img.save(filepath)
